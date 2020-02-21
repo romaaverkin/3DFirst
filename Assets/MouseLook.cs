9dc3c927441598edebaf92b35f620ef8fafc271d
Unknown <romaaverkin@gmail.com>
2020-02-21 14:37:23 +0700

    Первое копирование проекта

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public float sensitivityHor = 9.0f;
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
