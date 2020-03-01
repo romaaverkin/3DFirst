c307473fce6213e68fd69d6496adea1c6f81678c
Roman <romaaverkin@gmail.com>
2020-03-01 16:39:55 +0700

    Компонент CharacterController для распознавания столкновений

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
+    private CharacterController _charController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -17,6 +18,11 @@ public class FPSInput : MonoBehaviour
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
     }
 }
