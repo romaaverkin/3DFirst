3e5c87af112d840a5f37feac837ae3d145037475
Unknown <romaaverkin@gmail.com>
2020-03-02 15:02:06 +0700

    Ходить, а не летать

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
     private CharacterController _charController;
+    public float gravity = -9.8f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class FPSInput : MonoBehaviour
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
 
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
